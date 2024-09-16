093bf3c3d83369fe03857662870ac8db92519ead
Bruno Perez <brunoperez24793@gmail.com>
2024-09-16 15:01:36 -0300

    add gitignore

--- obj/Debug/net6.0/BudgetApp.AssemblyInfo.cs
+++ obj/Debug/net6.0/BudgetApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92543335f3cd85160779cfcdd970283451f3741c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
